73ffb5570d030cfb6576adb041eb938fead49c6e
kleget <shchegolkov.vova06@mail.ru>
2023-06-07 12:40:33 +0300

    ebani configggg

--- vk_script/config.py
+++ vk_script/config.py
@@ -3,6 +3,6 @@ list = 'main'
 # table_id = '1is-0lXTo439tsxuBMzBs-mhfJtxVWWZZtQVCGU3p31k' #основа
 # table_id = '1CjPFBSGAP4vWcTmnr9-miCNJGOcoVrAx55mMnJtchbo' #киря
 # pat = '/root/PROJECT/'
-pat = 'D:/PROJECT/'
+pat = '/root/PROJECT/'
 v = 5.131#
 sleep_time_on_error = 60
